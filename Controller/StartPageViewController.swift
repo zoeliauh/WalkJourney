006df9a28abb65a7b4989b65cf77375414ef42bc
zoeliauh <82745286+zoeliauh@users.noreply.github.com>
2021-10-31 16:49:08 +0800

    create lottie animation for profile page

--- Controller/StartPageViewController.swift
+++ Controller/StartPageViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class StartPageViewController: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var goButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tabBarController?.tabBar.isHidden = true
+        
+        logoImageView.layer.cornerRadius = logoImageView.frame.size.width / 2
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = false
     }
-    */
-
+    
+    @IBAction func goButtonPressed(_ sender: UIButton!) {
+        
+        guard let mapChosenViewController = UIStoryboard.position.instantiateViewController(withIdentifier: "MapChosenViewController") as? MapChosenViewController else { return }
+        
+        self.navigationController?.pushViewController(mapChosenViewController, animated: true)
+    }
 }
